b24fb292909c247c0a7c031a804e560e4267d781
unknown <aurimas0pupsys@gmail.com>
2022-03-20 21:54:58 +0200

    small adjustments to download more ticker data for variety

--- 2Indicators/indicators.py
+++ 2Indicators/indicators.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import numpy as np
 
+#data load up
+df_AAPL = yf.download('AAPL', period='1y', interval="1d")
+df_INTC = yf.download('INTC', period='1y', interval="1d")
+df_MSFT = yf.download('MSFT', period='1y', interval="1d")
 
 # WMA
+#buy/sell on price cross with wma, trend direction
+#buy lows on up, sell highs on down
 def WMA_calc(data, timePeriod):
     weights_mask = np.arange(1, timePeriod+1) #arange is not inclusive for last
     denom = weights_mask.sum() #can either sum all or do the whole n*(n+1)/2
@@ -27,15 +33,18 @@ def plot_WMA(df_price_to_plot, WMA_period, ticker):
     plt.title(ticker + " price with WMA")
     plt.ylabel("Price")
     plt.xlabel("Time")
+    plt.grid()
     plt.show()
 
 
-df = yf.download('AAPL', period='1y', interval="1d")
-plot_WMA(df.Close, 20, "AAPL")
+plot_WMA(df_AAPL.Close, 20, "AAPL")
+plot_WMA(df_INTC.Close, 20, "INTC")
+plot_WMA(df_MSFT.Close, 20, "MSFT")
 
 
 
 #MFI
+#overbought/oversold, divergences and failure swings
 def MFI_calc(data, timePeriod):
     MFI = np.zeros(len(data))
     typicalPrice = (data.High + data.Low + data.Close)/3
@@ -92,11 +101,15 @@ def plot_MFI(df_to_plot, MFI_period, ticker, MFI_upper, MFI_lower):
     plt.show()
 
 
-plot_MFI(df, 14, "AAPL", 80, 20)
+plot_MFI(df_AAPL, 14, "AAPL", 80, 20)
+plot_MFI(df_INTC, 14, "INTC", 80, 20)
+plot_MFI(df_MSFT, 14, "MSFT", 80, 20)
 
 
 
 #Keltner channel
+# either trend trade where follow candles outside bounds
+# or flat market trade where bounce between bounds
 def Keltner_calc(data, kc_multiplier, timePeriod):
     typicalPrice = (data.High + data.Low + data.Close)/3
 
@@ -130,4 +143,6 @@ def plot_Keltner(df_to_plot, ticker, kc_shift, period):
     plt.show()
 
 
-plot_Keltner(df, 'AAPL', 2, 20)
+plot_Keltner(df_AAPL, 'AAPL', 2, 20)
+plot_Keltner(df_INTC, 'INTC', 2, 20)
+plot_Keltner(df_MSFT, 'MSFT', 2, 20)
